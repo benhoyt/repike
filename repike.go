c89b143019894020344b55ad538d19f213bb8b7c
MoiTux <julie@moitu.xyz>
2022-08-14 11:39:55 +0200

    Just for the fun of it, let's remove an other 2 lines.
    This can be farfetch and even if it's valid go code, it's not a common one.
    I will not use this in a real situation.

--- repike.go
+++ repike.go
@@ -6,11 +6,10 @@ func Match(regexp, text string) bool {
 	if regexp != "" && regexp[0] == '^' {
 		return matchHere(regexp[1:], text)
 	}
-	for text != "" {
+	for ; text != ""; text = text[1:] {
 		if matchHere(regexp, text) {
 			return true
 		}
-		text = text[1:]
 	}
 	return matchHere(regexp, text) /* must look even if string is empty */
 }
@@ -32,11 +31,10 @@ func matchHere(regexp, text string) bool {
 
 // matchStar reports whether c*regexp matches at beginning of text.
 func matchStar(c byte, regexp, text string) bool {
-	for text != "" && (text[0] == c || c == '.') {
+	for ; text != "" && (text[0] == c || c == '.'); text = text[1:] {
 		if matchHere(regexp, text) {
 			return true
 		}
-		text = text[1:]
 	}
 	return matchHere(regexp, text) /* must look even if string is empty */
 }
